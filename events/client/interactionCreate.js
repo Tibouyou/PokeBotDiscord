07d85461cad317b95c9b534ab45887a46e241e4a
Tibouyou <tibouyou01@gmail.com>
2022-08-01 16:53:17 +0200

    add cd for owner

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -24,7 +24,7 @@ module.exports = {
       if(interaction.commandName == "donjon") {
         let cdMap = await Cooldown.findOne({name:"donjon"});
         if (cdMap.users.includes(interaction.user.id)) {
-          if (!interaction.user.id == ownerId) return interaction.reply('Vous devez attendre avant de pouvoir refaire cette commande');
+          return interaction.reply('Vous devez attendre avant de pouvoir refaire cette commande');
         } else {
           cdMap.users.push(interaction.user.id)
         }
