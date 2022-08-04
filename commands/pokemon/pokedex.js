6bb9ffcb0ed789bb4bb9e4c910cac38d941b232c
Tibouyou <tibouyou01@gmail.com>
2022-08-04 13:26:19 +0200

    add pokedex zone

--- commands/pokemon/pokedex.js
+++ commands/pokemon/pokedex.js
@@ -49,26 +49,79 @@ module.exports = {
   description: 'placeholder',
   options: [
     {
+      name: 'pokedex',
+      description: 'Choisir le type de pokédex',
+      type: 'STRING',
+      required: true,
+      choices: [
+        {
+          name: 'global',
+          value: 'global'
+        },
+        {
+          name: 'zone',
+          value: 'zone'
+        }
+      ]
+    },
+    {
+      name: 'zonenumber',
+      description: 'La zone à consulter',
+      type: 'NUMBER',
+      required: false,
+    },
+    {
       name: 'page',
       description: 'La page du pokédex à consulter',
       type: 'NUMBER',
       required: false,
-    },
+    }
   ],  
   async runInteraction(client, interaction) {
     const player = await Player.findOne({ id: interaction.user.id });
     const user = interaction.user;
-    const sumPoke = await Pokemon.estimatedDocumentCount() - 1;
-    const numberOfPages = Math.trunc(sumPoke / 10) + 1;
+    const pokedexType = interaction.options.getString('pokedex');
+    let sumPoke;
+    let numberOfPages;
     let currentPage;
-    if (interaction.options.getNumber('page')) {
-      if (interaction.options.getNumber('page') > numberOfPages) return interaction.reply('La page est trop grande');
-      if (interaction.options.getNumber('page') <= 0) return interaction.reply('La page est trop petite');
-      currentPage = interaction.options.getNumber('page')
+    if (pokedexType == "global") {
+      sumPoke = await Pokemon.estimatedDocumentCount() - 1;
+      numberOfPages = Math.trunc(sumPoke / 10) + 1;
+      currentPage;
+      if (interaction.options.getNumber('page')) {
+        if (interaction.options.getNumber('page') > numberOfPages) return interaction.reply('La page est trop grande');
+        if (interaction.options.getNumber('page') <= 0) return interaction.reply('La page est trop petite');
+        currentPage = interaction.options.getNumber('page')
+      } else {
+        currentPage = 1;
+      }
+      interaction.reply({embeds: [await embedPokedex(client, player, sumPoke, numberOfPages, currentPage, user)], components: [buttons] });
     } else {
-      currentPage = 1;
+      if (!interaction.options.getNumber('zonenumber')) return interaction.reply('Veuillez préciser une zone');
+      const pokemonList = await Pokemon.find({zone: interaction.options.getNumber('zonenumber')});
+      let listPokemon = [];
+      desc = '';
+      pokemonList.forEach( x => {
+        let pokemon;
+        if (player.pokemon.map(p => p.number).includes(x.number)) {
+          if (!player.pokemon.filter(pokemon => pokemon.number == x.number).filter(pokemon => pokemon.isShiny).length == 0) {
+            pokemon = player.pokemon.filter(pokemon => pokemon.isShiny).filter(pokemon => pokemon.number == x.number)[0];
+          } else {
+            pokemon = player.pokemon.filter(pokemon => pokemon.number == x.number)[0];
+          }
+          desc += `${(pokemon.isShiny) ? `${pokemon.emoji} ${pokemon.name} | ${pokemon.emojiShiny} :sparkles:` : `${pokemon.emoji} ${pokemon.name}`}\n`;
+        } else {
+          desc += `❔ Unknown\n`;
+        }
+      })
+      const embed = new MessageEmbed()
+        .setAuthor({ name: `${user.tag}`, iconURL: user.displayAvatarURL() })
+        .setColor('#cf102a')
+        .setThumbnail('https://media.discordapp.net/attachments/998631528563232798/999034290186748105/pokedex.png')
+        .setTimestamp()
+        .setDescription(desc)   
+      return interaction.reply({embeds: [embed]});
     }
-    interaction.reply({embeds: [await embedPokedex(client, player, sumPoke, numberOfPages, currentPage, user)], components: [buttons] });
     const message = await interaction.fetchReply();
     const collector = message.createMessageComponentCollector({ time: 60000 });
     collector.on('collect', async i => {
